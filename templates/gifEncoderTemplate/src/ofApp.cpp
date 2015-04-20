5af49b56f08465084d170afd6658e5d7fb193b30
Andrew McWilliams <andy@jahya.net>
2015-04-20 00:10:11 -0400

    Switches to rendering from an ofFbo

--- templates/gifEncoderTemplate/src/ofApp.cpp
+++ templates/gifEncoderTemplate/src/ofApp.cpp
@@ -1,41 +1,62 @@
 #include "ofApp.h"
 
+/* This draws the animation */
+void ofApp::setupAnim() {
+    vid.initGrabber(width, height);
+}
+
+void ofApp::updateAnim(){
+    vid.update();
+}
+
+void ofApp::drawAnim() {
+    vid.draw(0, 0);
+    if(ofGetFrameNum() % 2 == 0) {
+        ofDrawBitmapString(ofToString(ofGetFrameNum()), 10, 20);
+    }
+}
+
+/* This handles the setup and GIF rendering, etc */
 void ofApp::setup(){
     //Gif render values
     renderMode = true;
     width = 500, height = 500;
     duration = .03, colors = 256;
+    saveOnFrame = 20;
+
     filename = "render.gif";
     framerate = renderMode ? 5 : 60;
-    saveOnFrame = 20;
     
     //Init
     ofSetFrameRate(framerate);
     ofSetWindowShape(width, height);
     gifEncoder.setup(width, height, duration, colors);
     
+    fbo.allocate(width, height, GL_RGB);
+    fbo.begin();
+    ofClear(255, 255, 255, 0);
+    fbo.end();
+
     //Begin patch
-    ofBackground(50, 40, 50);
-    vid.initGrabber(width, height);
+    setupAnim();
 }
 
 void ofApp::update(){
-    vid.update();
+    updateAnim();
 }
 
 void ofApp::draw(){
-    vid.draw(0, 0);
-    if(ofGetFrameNum() % 2 == 0) {
-        ofDrawBitmapString(ofToString(ofGetFrameNum()), 10, 20);
-    }
+    fbo.begin();
+    drawAnim();
+    fbo.end();
     captureFrame();
+    fbo.draw(0, 0);
 }
 
 void ofApp::captureFrame() {
-
-    gifEncoder.addFrame(vid.getPixels(), width, height,
-                        vid.getPixelsRef().getBitsPerPixel(),
-                        duration);
+    fbo.readToPixels(pixels);
+    gifEncoder.addFrame(pixels.getPixels(), width, height,
+                        pixels.getBitsPerPixel(), duration);
     
     if(ofGetFrameNum() == saveOnFrame) {
         gifEncoder.save(filename);
--- templates/gifEncoderTemplate/src/ofApp.h
+++ templates/gifEncoderTemplate/src/ofApp.h
@@ -5,31 +5,35 @@
 
 class ofApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+    void setup();
+    void setupAnim();
+    void update();
+    void updateAnim();
+    void draw();
+    void drawAnim();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-    
-        void onGifSaved(string & fileName);
-        void exit();
-        void captureFrame();
-    
-        int width, height, colors, duration;
-        int framerate, saveOnFrame;
-        string filename;
-        bool renderMode;
-    
-        ofVideoGrabber vid;
-        ofxGifEncoder gifEncoder;
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
 
+    void onGifSaved(string & fileName);
+    void exit();
+    void captureFrame();
+
+    int width, height, colors, duration;
+    int framerate, saveOnFrame;
+    string filename;
+    bool renderMode;
+
+    ofVideoGrabber vid;
+    ofxGifEncoder gifEncoder;
+    ofFbo fbo;
+    ofPixels pixels;
 };

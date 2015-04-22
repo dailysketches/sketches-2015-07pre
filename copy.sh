63156e463e195d7a6cad83e24a5133c3bdb0c4b9
Andy McWilliams <andy@jahya.net>
2015-04-21 23:28:43 -0400

    Removes old shell file

--- copy.sh
+++
@@ -1,3 +0,0 @@
-cp -rf ../openFrameworks/versions/084/apps/dailySketchesTemplates/ templates
-cp -rf ../openFrameworks/versions/084/apps/dailySketches/ sketches
-cp -f sketches/*/bin/data/*.gif dailysketches.github.io/app/img/sketches/

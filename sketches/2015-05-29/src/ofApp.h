4b90349f31f05beaf3679548cbb54b110368da76
Andrew McWilliams <andy@jahya.net>
2015-05-29 23:01:58 -0400

    Adds sketch 2015-05-29

---
+++ sketches/2015-05-29/src/ofApp.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGifEncoder.h"
+
+class ofApp : public ofBaseApp{
+
+public:
+    void setup();
+    void setupAnim();
+    void update();
+    void updateAnim();
+    void draw();
+    void drawAnim();
+    void renderGif();
+    void rotateScreen(float degrees);
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+    void exit();
+    void captureFrame();
+
+    int width, height, halfWidth, halfHeight, colors, duration;
+    int framerate, saveOnFrame;
+    string filename, renderMessage;
+    bool slowMode, renderingNow, paused;
+
+    ofxGifEncoder gifEncoder;
+    ofFbo fbo;
+    ofPixels pixels;
+    float x, y;
+    int maskOpacity;
+};

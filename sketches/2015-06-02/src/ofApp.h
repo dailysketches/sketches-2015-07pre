d1ee419018ae17926843f8583c21b4950fb5b6d4
Andrew McWilliams <andy@jahya.net>
2015-06-03 18:06:58 -0500

    Adds sketch 2015-06-02

---
+++ sketches/2015-06-02/src/ofApp.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxBpm.h"
+#include "ofxAudioUnit.h"
+#include "TALNoiseMakerChain.h"
+#include "ofxAudioUnitManager.h"
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void play();
+    void togglePlaying();
+    void exit();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    ofxAudioUnitManager manager;
+    TALNoiseMakerChain chain1, chain2;
+    ofxBpm bpm;
+    bool playing;
+    int note, currentChain;
+    
+    float val;
+};

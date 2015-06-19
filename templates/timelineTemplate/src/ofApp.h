db01d2e020cb8e60f051a9ce294a3462bc570efd
Andrew McWilliams <andy@jahya.net>
2015-06-19 19:56:21 -0400

    Adds timeline template

---
+++ templates/timelineTemplate/src/ofApp.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGifEncoder.h"
+#include "ofxTimeline.h"
+
+class ofApp : public ofBaseApp{
+
+public:
+    void setup();
+    void setupAnim();
+    void update();
+    void updateAnim();
+    void draw();
+    void drawAnim();
+    void renderGif();
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+    void exit();
+    void captureFrame();
+
+    int width, height, halfWidth, halfHeight, colors, duration;
+    int framerate, saveOnFrame;
+    string filename, renderMessage;
+    bool slowMode, renderingNow, paused, fullscreen, showTimeline;
+
+    ofxGifEncoder gifEncoder;
+    ofFbo fbo;
+    ofPixels pixels;
+
+    ofxTimeline timeline;
+    void bang(ofxTLBangEventArgs &args);
+    int size;
+    string text;
+};
